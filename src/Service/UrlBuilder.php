7b25b4c9a7790bcf9501c3ff99977ed4f5f18e3a
Shamrylo Mykola <itlutsk@gmail.com>
2022-03-17 15:32:37 +0200

    Add first service UrlBuilder 2022-03-17

---
+++ src/Service/UrlBuilder.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class UrlBuilder
+{
+    private $requestStack;
+    private $request;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+        $this->request = $this->requestStack->getCurrentRequest();
+    }
+
+    public function getFullUrl(string $url): string
+    {
+        if(!empty($url))
+            return $this->request->server->get('REQUEST_SCHEME') . '://' . $this->request->server->get('HTTP_HOST') . $url;
+
+        return '';
+    }
+}
