df600a462500c1abf08599e815515a84b1f05097
Shamrylo Mykola <itlutsk@gmail.com>
2022-03-20 22:56:19 +0200

    Form data processing 2022-03-20

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\BlogPost;
 use App\Form\BlogPostType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AboutController extends AbstractController
 {
     #[Route('/about', name: 'app_about')]
-    public function index(): Response
+    public function index(RequestStack $request): Response
     {
+        $post = new BlogPost();
+
         $form = $this->createForm(BlogPostType::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $post = $form->getData();
+
+            return $this->redirectToRoute('about');
+        }
 
         return $this->render('about/index.html.twig', [
             'controller_name' => 'AboutController',
