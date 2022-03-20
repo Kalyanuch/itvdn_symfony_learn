21ac67228f1d1f4fd2feb89f48fa323f9f689f96
Shamrylo Mykola <itlutsk@gmail.com>
2022-03-20 22:13:13 +0200

    Add another form 2022-03-20

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\BlogPostType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,11 @@ class AboutController extends AbstractController
     #[Route('/about', name: 'app_about')]
     public function index(): Response
     {
+        $form = $this->createForm(BlogPostType::class);
+
         return $this->render('about/index.html.twig', [
             'controller_name' => 'AboutController',
+            //'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/BlogPostType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\BlogPost;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BlogPostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('status')
+            ->add('category')
+            ->add('save', SubmitType::class, ['label' => 'Save new post'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => BlogPost::class,
+        ]);
+    }
+}
