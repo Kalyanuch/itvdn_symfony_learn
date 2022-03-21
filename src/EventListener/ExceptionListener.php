ddb314bb5ecb540611bbc47eea7a7ec766e582a6
Shamrylo Mykola <itlutsk@gmail.com>
2022-03-21 12:08:33 +0200

    Add event listener for kernel exceptions 2022-03-21

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+
+class ExceptionListener
+{
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $exception = $event->getThrowable();
+
+        $message = 'Error: ' . $exception->getCode() . ' ' . $exception->getMessage();
+
+        $event->setResponse((new Response())->setContent($message));
+    }
+}
